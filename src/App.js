108d9b18fa131b4bce9bea01ad444b39ff12bfdd
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2023-05-24 12:35:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./Pages/About";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {Routes,Route} from "react-router-dom";
+
+
+
+const App = () =>{
+
+  return(
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/a" element={<About />} />
+        <Route path="/b" element={<Blog />} />
+        <Route path="/c" element={<Contact />} />
+
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+
+const Home=()=>{
+
+
+    return(
+        <div>
+          <h1>Home</h1>
+<p>this is home page</p>
+
+
+        </div>
+    )
+}
+export default Home;
